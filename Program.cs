fc8b969d0a2f34789025c1a9469c2238cfd824ee
Violetta <94271862+burlakova-veta@users.noreply.github.com>
2023-05-11 20:42:02 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace csharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int num = int.Parse(Console.ReadLine());
+            int s = 0;
+            while (num > 0)
+            {
+                s = s + num % 10;
+                num = num / 10;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
